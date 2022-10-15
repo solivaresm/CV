7cf17f758bc4684d18689471d109940830163426
solivaresm <86017789+solivaresm@users.noreply.github.com>
2022-10-15 16:10:33 -0700

    first edits, learning how to use the template

--- 2021-04-17-FCC-CV-Workshop/CV/r/data_blank.r
+++ 2021-04-17-FCC-CV-Workshop/CV/r/data_blank.r
@@ -1,3 +1,18 @@
+edu <- tribble(
+    ~degree, ~startYear, ~endYear, ~inst, ~where, ~detail,
+   "Bachelors Environmental Studies", 2018, 2022, "Haverford College", "Philadelphia,PA",NA, 
+   "Diploma", 2014, 2018, "University School of Nashville", "Nashville, TN", NA
+
+)
+
+work <- tribble(
+    ~title, ~unit, ~startMonth, ~startYear, ~endMonth, ~endYear, ~where, ~detail,
+    "GIS Assistant", "Sonoran Desert Inventory & Monitoring Network | Scientists in the Parks Intern | Conservation Legacy ", "June", "2022", NA, "Present", "Tucson, AZ", "PROGRAMS USED: R, Rmarkdown, HTML, GitHub, ArcGIS Field Maps", 
+    "GIS Assistant", "Sonoran Desert Inventory & Monitoring Network | Scientists in the Parks Intern | Conservation Legacy ", "June", "2022", NA, "Present", "Tucson, AZ", "Data management intern with the Southwest Network Collaborative resposinble for writing quality checks, cleaning data, and developing a data release report for the Big Rivers Protocol.",
+    "GIS Assistant", "Sonoran Desert Inventory & Monitoring Network | Scientists in the Parks Intern | Conservation Legacy ", "June", "2022", NA, "Present", "Tucson, AZ", "Develops and facilitates monthly R Trainings for the network covering topics such as foundations in RStudio/RMarkdown, data wrangling, reproducability, version control, and functional programming.", 
+    "GIS Assistant", "Sonoran Desert Inventory & Monitoring Network | Scientists in the Parks Intern | Conservation Legacy ", "June", "2022", NA, "Present", "Tucson, AZ", "Occasional fieldwork for the vegetation, wildlife, and streams protocols in addition to IT opperations support."
+)
+
 projects <- tribble(
     ~area, ~accomplishment, ~year, ~where, ~detail,
 )
@@ -14,13 +29,6 @@ honors <- tribble(
     ~area, ~accomplishment, ~year, ~where, ~detail,
 )
 
-edu <- tribble(
-    ~degree, ~startYear, ~endYear, ~inst, ~where, ~detail,
-)
-
-work <- tribble(
-    ~title, ~unit, ~startMonth, ~startYear, ~endMonth, ~endYear, ~where, ~detail,
-)
 
 works <- tribble(
     ~area, ~accomplishment, ~year, ~where, ~detail,
