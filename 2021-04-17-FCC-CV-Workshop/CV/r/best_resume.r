72079eb1dacbfebaf3f9e91adab60023e278b512
solivaresm <86017789+solivaresm@users.noreply.github.com>
2022-10-16 23:01:22 -0700

    best_resume, concise_resume and awesome_cv file creations, addition of section headers and editing of Rmd

---
+++ 2021-04-17-FCC-CV-Workshop/CV/r/best_resume.r
@@ -0,0 +1,72 @@
+edu <- tribble(
+    ~degree, ~startYear, ~endYear, ~inst, ~where, ~detail,
+   "Bachelors Environmental Studies", 2018, 2022, "Haverford College", "Philadelphia,PA",NA, 
+   "Diploma", 2014, 2018, "University School of Nashville", "Nashville, TN", NA
+
+)
+
+work <- tribble(
+    ~title, ~unit, ~startMonth, ~startYear, ~endMonth, ~endYear, ~where, ~detail,
+    "GIS Assistant", "Sonoran Desert Inventory & Monitoring Network | Scientists in the Parks Intern | Conservation Legacy ", "June", 2022, NA, NA, "Tucson, AZ", "PROGRAMS USED: R, Rmarkdown, HTML, GitHub, ArcGIS Field Maps", 
+    "GIS Assistant", "Sonoran Desert Inventory & Monitoring Network | Scientists in the Parks Intern | Conservation Legacy ", "June", 2022, NA, NA, "Tucson, AZ", "Data management intern with the Southwest Network Collaborative resposinble for writing quality checks, cleaning data, and developing a data release report for the Big Rivers Protocol.",
+    "GIS Assistant", "Sonoran Desert Inventory & Monitoring Network | Scientists in the Parks Intern | Conservation Legacy ", "June", 2022, NA, NA, "Tucson, AZ", "Develops and facilitates monthly R Trainings for the network covering topics such as foundations in RStudio/RMarkdown, data wrangling, reproducability, version control, and functional programming.", 
+    "GIS Assistant", "Sonoran Desert Inventory & Monitoring Network | Scientists in the Parks Intern | Conservation Legacy ", "June", 2022, NA, NA, "Tucson, AZ", "Occasional fieldwork for the vegetation, wildlife, and streams protocols in addition to IT opperations support.", 
+    "Student Researcher", "Bryn Mawr College Biology Department", "August", 2021, "May", 2022, "Bryn Mawr, PA", "TO DO",
+    "REU Cohort Member", "Harvard Forest", "May", 2021, "August", 2022, "Remote", "TO DO",
+    "Access and Diversity Intern | Admissions Fellow | Student Admissions Advisory Committee Member", "Haverford College Office of Admissions", "August", 2019, "May", 2022, "Ardmore, PA", "TO DO", 
+    "Student Worker", "Haverford College Administrative Auxilliary Services", "August", 2019, "May", 2022, "Ardmore, PA", "TO DO",
+    "Seasonal Naturalist", "Warner Parks Nature Center", "May", 2020, "August", 2020, "Nashville, TN", "TO DO", 
+    "Conservation Intern", "Radnor to River", "May", 2020, "August", 2020, "Remote | Nashville, TN", "TO DO",
+    "Teacher's Assistant", "Horizons at University School of Nashville", "Summers", 2017, NA, 2019, "Nashville, TN", "TO DO", 
+)
+honors <- tribble(
+    ~area, ~accomplishment, ~year, ~where, ~detail,
+    "Cum Laude Society", "TO DO", 2022, "Haverford College", NA,
+    "Center for Peace and Global Citizenship Fellow", "TO DO", 2020, "Haverford College", NA,
+    "Magill-Rhoads Scholar", "TO DO", 2018, "Haverford College", NA,
+    "Cum Laude Society", "TO DO", 2018, "University School of Nashville", NA
+)
+
+## change to start year end year start month etc. 
+leadership <- tribble(
+    ~area, ~accomplishment, ~year, ~where, ~detail,
+    "Haverford College", "Officer of Multiculturalism", 2021, "Student Council", "TO DO",
+    "Haverford College", "Co-President | Senior Advisor | Bi-College Representative", 2019, "Pulso Latino Dance Troupe", "TO DO", 
+    "Haverford College", "Board Member | Latnix Center Committee and Resident Advisor ", 2019, "Alliance of Latin American Students", "TO DO", 
+    "Haverford College", "Volunteer | Class Instructor", 2018, "Haverfarm", "TO DO" ,
+    "Haverford College", "Co-President", 2020, "Environmental House", "TO DO", 
+    "Haverford College", "Residential Advisor", 2018, "Customs Program", "TO DO", 
+    "Haverford College", "Green Squirrel Advocate", 2018, "Committee for Environmental Responsibility", "TO DO", 
+    "Tutoring", "Math, English, and Spanish Tutor", 2014, "Front Lines Tutoring | Bethel Academy | Big Brothers Big Sisters | Beyond Edgehill", "TO DO",
+    "Communications", "Social Media Coordinator", 2022, "Society for Conservation GIS", "TO DO", 
+    "Commmunications", "Social Media Coordinator", 2022, "Climbing in Color", "TO DO"
+)
+skills <- tribble(
+    ~area, ~skills,
+    "Programming Languages", "R, Python, Basic SQL",
+    "Geographic Information Systems", "QGIS, ESRI Suite | ArcMap, ArcGIS Pro, Field Maps, Survey 123, Story Maps, Dashboards",
+    "Markup Languages", "HTML, RMarkdown, Basic LaTeX", 
+    "Version Control", "GIT,Github Desktop",
+    "Software", "RStudio, Microsoft Office Suite | Excel, Outlook, OneNote, Powerpoint, Word"
+)
+trainings <- tribble(
+    ~area, ~accomplishment, ~year, ~where, ~detail,
+    "Programming Languages", "R", 2022, "Sonoran Desert Network", "TO DO"
+)
+coursework <- tribble(
+    ~area, ~accomplishment, ~year, ~where, ~detail,
+    "Haverford College", NA, NA, NA, NA,
+    "Professional Development", NA, NA, NA, NA,
+    "ESRI", NA, NA, NA, NA
+)
+
+certifications <- tribble(
+    ~area, ~accomplishment, ~year, ~where, ~detail,
+)
+
+
+
+
+
+
+
---
+++ 2021-04-17-FCC-CV-Workshop/CV/r/concise_resume.r
@@ -0,0 +1,6 @@
+edu <- tribble(
+  ~degree, ~startYear, ~endYear, ~inst, ~where, ~detail,
+  "Bachelors Environmental Studies", 2018, 2022, "Haverford College", "Philadelphia,PA",NA, 
+  "Diploma", 2014, 2018, "University School of Nashville", "Nashville, TN", NA
+  
+)
